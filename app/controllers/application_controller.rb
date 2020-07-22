9d33316dff437d95fac378201b28fdc62fc4ba96
root <root@LAPTOP-90D0NS7H.localdomain>
2020-07-22 08:58:11 +0200

    eslint changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::API
     end
 
     def not_authorized
-      render json: { error: 'Not authorizsssssed' }, status: :unauthorized
+      render json: { error: 'Cannot find email/password combination' }, status: :unauthorized
     end
 end
